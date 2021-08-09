47f1fc8487b7e1a6c462e0be1d07f9d0aa134938
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-09 12:39:17 +0530

    [Devesh]: Refractored employee Part time wage calculation feature

--- src/com/bridgelabz/employeewage/EmployeeWageComp.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComp.java
@@ -1,14 +1,21 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWageComp {
-	public static final int IS_Time = 1;   // variable for checking employee present or not
+	
+	public static final int IS_Full_Time = 1;   // variable for checking employee worked full time
 	public static final int Wage_per_hour = 20; //variable for wage per hour
+	public static final int IS_Part_Time = 2; //variable for checking employee worked part time
+	
 	public static void main(String[] args){
 		
 		int ehours=0; //variable for total hours worked by employee
 		int ewage =0; //variable for total wage considering hours worked by employee
-		int poa = (int)((Math.random()*10)%2);
-		if (poa==IS_Time) ehours=8;
+		
+		int echeck = (int)((Math.random()*10)%3);
+		
+		if (echeck==IS_Full_Time) ehours=8;
+		else if (echeck == IS_Part_Time) ehours =4;
+		
 		ewage = ehours*Wage_per_hour;
 		System.out.println("The daily Employee wage is "+ ewage);
 	}
