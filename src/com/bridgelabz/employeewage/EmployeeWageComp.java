cd3b8051d3a889e60185e79c8cbd66bd466c9503
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-09 12:03:47 +0530

    [Devesh]: Added employee attendance feature

--- src/com/bridgelabz/employeewage/EmployeeWageComp.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComp.java
@@ -1,8 +1,9 @@
 package com.bridgelabz.employeewage;
 
 public class EmployeeWageComp {
+	public static final int IS_Time = 1;   // variable for checking employee present or not
 	public static void main(String[] args){
-		int IS_Time = 1;   // variable for checking employee present or not
+		
 		int poa = (int)((Math.random()*10)%2);
 		if (poa==IS_Time) System.out.println("Employee is present");
 		else System.out.println("Employee is absent");
