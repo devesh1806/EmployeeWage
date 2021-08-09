019bf93c77cb0eccb50b77bb6f01a2cae98a2f4e
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-09 12:21:57 +0530

    [Devesh]: Refractored employee Daily wage calculation feature

--- src/com/bridgelabz/employeewage/EmployeeWageComp.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComp.java
@@ -2,10 +2,14 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWageComp {
 	public static final int IS_Time = 1;   // variable for checking employee present or not
+	public static final int Wage_per_hour = 20; //variable for wage per hour
 	public static void main(String[] args){
 		
+		int ehours=0; //variable for total hours worked by employee
+		int ewage =0; //variable for total wage considering hours worked by employee
 		int poa = (int)((Math.random()*10)%2);
-		if (poa==IS_Time) System.out.println("Employee is present");
-		else System.out.println("Employee is absent");
+		if (poa==IS_Time) ehours=8;
+		ewage = ehours*Wage_per_hour;
+		System.out.println("The daily Employee wage is "+ ewage);
 	}
 }
