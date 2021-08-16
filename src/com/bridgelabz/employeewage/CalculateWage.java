164bbc32b7694adb9f1b9d488f5647bfe2fedf0b
devesh1806 <42887196+devesh1806@users.noreply.github.com>
2021-08-16 10:14:17 +0530

    [Devesh]: Refractored code to add ability to compute employee wage for multiple companies using OOPS concept

--- src/com/bridgelabz/employeewage/CalculateWage.java
+++ src/com/bridgelabz/employeewage/CalculateWage.java
@@ -1,17 +1,17 @@
 package com.bridgelabz.employeewage;
 
 public class CalculateWage {
+	
 	public static final int IS_Full_Time = 1;   // variable for checking employee worked full time
-	public static final int Wage_per_hour = 20; //variable for wage per hour
 	public static final int IS_Part_Time = 2; //variable for checking employee worked part time
 	
-	public static void compute() {
+	CalculateWage(String compname, int Wage_per_hour, int days, int hours) {
 		int totalhours= 0 ; //variable for total hours worked by employee
 		int emphrs = 0 ; //variable for hours worked by employee in day
 		
 		int totalwdays = 0; //variable for working days 
 		
-		while ( totalwdays < 20 && totalhours < 100 ) {  //condition for 20 days a month.
+		while ( totalwdays < days && totalhours < hours ) {  
 			
 			totalwdays += 1;
 			int echeck = (int)((Math.random()*10) % 3);
@@ -31,10 +31,9 @@ public class CalculateWage {
 			}
 			
 			totalhours += emphrs;
-			System.out.println("Day = " + totalwdays + " and Employee hour = " + emphrs );
 			
 		}
 		int totalempwage = totalhours * Wage_per_hour ; 
-		System.out.println("Total Employee wage is " + totalempwage );
+		System.out.println("Total Employee wage earned in " + compname + " is " + totalempwage );
 	}
 }
--- src/com/bridgelabz/employeewage/EmployeeWageComp.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComp.java
@@ -4,6 +4,12 @@ public class EmployeeWageComp {
 	
 	public static void main(String[] args){
 		
-		CalculateWage.compute();
+		//company1
+		CalculateWage cw1 = new CalculateWage("Reliance",20,20,100);
+		
+		//company2
+		CalculateWage cw2 = new CalculateWage("Capgemini",25,22,110);
+		
+		
 	}
 }
